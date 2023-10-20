4312a8716a246f84c6d562de41bbccf2883643ee
Ramenable <abelisable2@gmail.com>
2023-10-20 18:45:09 -0400

    Create Agora website stub and app router

---
+++ src/app/About/page.tsx
@@ -0,0 +1,12 @@
+
+import styles from './page.module.css'
+
+export default function About() {
+    return (
+    <main className={styles.main}>
+      <div>
+        <h1>About page!</h1>
+      </div>
+    </main>
+    )
+}
