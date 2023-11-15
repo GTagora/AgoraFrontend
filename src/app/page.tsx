a420a8e903cb56c79b011ff56ef61062bf21907e
ohu9 <oliviah221@gmail.com>
2023-11-15 16:56:10 -0500

    initial home page set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from './page.module.css'
+// import './reference.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.center}>
+      <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,600;0,700;1,400&family=EB+Garamond:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Playfair+Display+SC:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
+      <div className={styles.heading}>
         <Image
           className={styles.logo}
-          src="/logo-black.png"
-          width={150}
-          height={150}
+          src="/logo-black-box.png"
+          width={100}
+          height={100}
           alt='Agora logo'
         />
+        <h1>AGORA</h1>
+        <h2>Georgia Tech’s Journal of Christian Thought</h2>
       </div>
 
       <div className={styles.grid}>
