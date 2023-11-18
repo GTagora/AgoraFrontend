3f8012ef6a1c2ff041e20da8bfb767667589b7c7
ohu9 <oliviah221@gmail.com>
2023-11-18 11:55:29 -0500

    reorganized font imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,38 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from './page.module.css'
-import { EB_Garamond, Playfair_Display_SC, Cormorant_Garamond } from '@next/font/google'
-
-const eb_garamond = EB_Garamond( {
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  variable: '--font-ebgaramond',
-  display: 'swap'
-})
-
-const playfair = Playfair_Display_SC({
-  weight: ['400', '700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  variable: '--font-playfair',
-  display: 'swap'
-})
-
-const cormorant = Cormorant_Garamond({
-  weight: ['400', '500', '700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  variable: '--font-cormorant',
-  display: 'swap'
-})
 
 export default function Home() {
   return (
-    <html className={`${eb_garamond.variable} ${playfair.variable} ${cormorant.variable}`}>
   <main className={styles.main}>
-        <div className={styles.heading}>
-          <Image
-            className={styles.logo}
-            src="/logo-black-box.png"
-            width={100}
-            height={100}
-            alt='Agora logo'
-          />
-          <h1>AGORA</h1>
-          <h2>Georgia Tech’s Journal of Christian Thought</h2>
-        </div>
+    <div className={styles.heading}>
+      <Image
+        className={styles.logo}
+        src="/logo-black-box.png"
+        width={100}
+        height={100}
+        alt='Agora logo'
+      />
+      <h1>AGORA</h1>
+      <h2>Georgia Tech’s Journal of Christian Thought</h2>
+    </div>
 
-        {/* <div className={styles.grid}>
-          <Link href="/About" className={styles.card}>
-            <h2>
-              About <span>-&gt;</span>
-            </h2>
-            <p>Placeholder Text</p>
-          </Link>
+    {/* <div className={styles.grid}>
+      <Link href="/About" className={styles.card}>
+        <h2>
+          About <span>-&gt;</span>
+        </h2>
+        <p>Placeholder Text</p>
+      </Link>
 
-          <Link href="/Blog" className={styles.card}>
-            <h2>
-              Blog <span>-&gt;</span>
-            </h2>
-            <p>Placeholder Text</p>
-          </Link>
-        </div> */}
-      </main>
-    </html>
+      <Link href="/Blog" className={styles.card}>
+        <h2>
+          Blog <span>-&gt;</span>
+        </h2>
+        <p>Placeholder Text</p>
+      </Link>
+    </div> */}
+  </main>
     
   )
 }
