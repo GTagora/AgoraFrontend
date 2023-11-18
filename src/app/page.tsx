bb1eaf9f52a68391a64481ffcb3ca705049f1859
ohu9 <oliviah221@gmail.com>
2023-11-18 11:52:22 -0500

    figuring out some routing stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from './page.module.css'
-import Navbar from '@/app/navbar'
+// import Navbar from '@/app/navbar'
 // import './reference.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,600;0,700;1,400&family=EB+Garamond:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Playfair+Display+SC:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
-      <Navbar/>
       <div className={styles.heading}>
         <Image
           className={styles.logo}
