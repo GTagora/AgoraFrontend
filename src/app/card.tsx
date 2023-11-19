3419d8fe424a55f2e10518a7f401489479a2cf1a
ohu9 <oliviah221@gmail.com>
2023-11-19 11:10:52 -0500

    added article cards

--- src/app/card.tsx
+++ src/app/card.tsx
@@ -2,7 +2,7 @@
 import styles from './page.module.css'
 
 
-export default function Card({title = "title", genre = "genre"}) {
+export default function Card({title = "title", genre = "GENRE"}) {
     return (
         <div className={styles.card}>
             <p>{genre}</p>
