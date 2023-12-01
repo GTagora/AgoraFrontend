8ac27185dae2fe35a8e4d4eb40af5ce7d978f3fc
ohu9 <oliviah221@gmail.com>
2023-12-01 13:25:08 -0500

    set up not-found-page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,13 @@
+import Link from 'next/link'
+import styles from './page.module.css'
+
 export default function NotFound() {
     return (
-        <div>Page Not Found</div>
+        <div>
+            <h2>There was a problem.</h2>
+            <p>We could not find the page you were looking for.</p>
+            <Link href="/contact">
+            <div className={styles.button}>&#x2192;&nbsp;&nbsp;&nbsp;&nbsp;stay up to date</div>
+        </Link>
+        </div>
 )}
