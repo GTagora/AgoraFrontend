f6101fd6284caf6dded6e7eb54e4de60fae3e759
ohu9 <oliviah221@gmail.com>
2023-12-05 02:51:56 -0500

    fixed merge conflicts

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,9 +1,5 @@
 import styles from './page.module.css'
-<<<<<<< HEAD
-=======
-import Header from '@/app/components/header'
 import Footer from '../../components/footer'
->>>>>>> b4f9dd6066ce7c33572f00bbff8d7a57d35fa66b
 
 export default function About() {
     return (
