7e33928b434fb54b0a3058532a0cb262d9b59a95
ohu9 <oliviah221@gmail.com>
2024-04-22 13:00:53 -0400

    style updates for mobile view issue card

--- dist/server/webpack-runtime.js
+++ dist/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("c3e9b1c7b2427954")
+/******/ 		__webpack_require__.h = () => ("d413f332715b907b")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "609bdaf0896d9745"; }
+/******/ 		__webpack_require__.h = function() { return "bc4510e66417cad0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
