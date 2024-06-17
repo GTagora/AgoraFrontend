be896c079684f7aa88ff79baf9bd9f5dc4d1286f
ohu9 <oliviah221@gmail.com>
2024-06-17 16:52:28 -0400

    format blog page

--- src/app/(pages)/blog/page.tsx
+++ src/app/(pages)/blog/page.tsx
@@ -13,16 +13,11 @@ export default async function Blog() {
     const articles = await getBlogArticles();
     return (
     <main className={styles.main}>
-        <div className={styles.comingsoon}>
-            <h1>Blog</h1>
-            <p>Content coming summer &apos;24!</p><br/>
-            <p>The blog features pieces outside the scope of semesterly issue themes and are published intermittently throughout the year.</p>
-            <Link href="/contact">
-                <div className={styles.button}>&#x2192;&nbsp;&nbsp;&nbsp;&nbsp;stay notified</div>
-            </Link>
-        </div> 
-        <div className={styles.footer}>
-            <Footer />
+        <h1>Blog</h1>
+        <div className={styles.allcards}>
+            {articles && articles.map(async (article: any) => (
+                <BlogCard key={null} article={article}></BlogCard>
+            ))}
         </div>
     </main>
     )
@@ -30,12 +25,14 @@ export default async function Blog() {
 
 
 {/* COMING SOON BOX
-<div className={styles.container}>
-    <p>Content coming Spring &apos;24!</p>
+<div className={styles.comingsoon}>
+    <h1>Blog</h1>
+    <p>Content coming summer &apos;24!</p><br/>
+    <p>The blog features pieces outside the scope of semesterly issue themes and are published intermittently throughout the year.</p>
     <Link href="/contact">
-        <div className={styles.button}>&#x2192;&nbsp;&nbsp;&nbsp;&nbsp;stay up to date</div>
+        <div className={styles.button}>&#x2192;&nbsp;&nbsp;&nbsp;&nbsp;stay notified</div>
     </Link>
-</div> */}
+</div>  */}
 
 {/* CONTENT 
 <main className={styles.main}>
