400579a8411d01afbde63e48aa52919fd4b6b613
ohu9 <oliviah221@gmail.com>
2024-06-19 16:33:04 -0400

    a bench

--- src/app/(pages)/blog/page.tsx
+++ src/app/(pages)/blog/page.tsx
@@ -1,5 +1,6 @@
 import styles from './page.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 import Footer from '@/app/components/footer'
 import BlogCard from '@/app/(templates)/blog-card'
 
@@ -13,11 +14,29 @@ export default async function Blog() {
     const articles = await getBlogArticles();
     return (
     <main className={styles.main}>
-        <h1>Blog</h1>
+        <h1 className={styles.h1}>Blog</h1>
         <div className={styles.allcards}>
-            {articles && articles.map(async (article: any) => (
-                <BlogCard key={null} article={article}></BlogCard>
-            ))}
+            {/* {articles && articles.map(async (article:any, i:number) => (
+                <BlogCard key={i} article={article}></BlogCard>
+            ))} */}
+            <Link href={"blog/a-bench"}>
+                <div className={styles.card}>
+                    <Image 
+                        className={styles.cardImage}
+                        src="https://firebasestorage.googleapis.com/v0/b/agora-ca31b.appspot.com/o/Piece%20Title%20Cards%2FBlog%2Fa_bench.jpeg?alt=media&token=09e1c89b-63a7-458e-b9cc-c92f130e02dc"
+                        width={400}
+                        height={400}
+                        alt='article image'/>
+                    <div className={styles.box}>
+                        <div className={styles.hbox}>
+                            <h2>a bench</h2><h3>POEM</h3>
+                        </div>
+                        <h4>06.19.2024</h4>
+                    </div>
+                    {/* <p className={styles.preview} dangerouslySetInnerHTML={{ __html: styles.text.replace(/\\n/g, '\n')}}></p> */}
+                    
+                </div>
+            </Link>
         </div>
     </main>
     )
