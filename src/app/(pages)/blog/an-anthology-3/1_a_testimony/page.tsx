9410b2610c787a2467e20410531fa25cb5a26f6e
ohu9 <oliviah221@gmail.com>
2025-03-03 13:05:44 -0500

    fix image source

--- src/app/(pages)/blog/an-anthology-3/1_a_testimony/page.tsx
+++ src/app/(pages)/blog/an-anthology-3/1_a_testimony/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
 					</Link>
 					<Image
 						className={styles.image}
-						src="/blog/an-anthology-3/a testimony.jpg"
+						src="/blog/an-anthology-3/a testimony.png"
 						width={600}
 						height={600}
 						alt="a testimony article image"
