d0f871c491a7d870da2834f2122236b6b81b85c7
ohu9 <oliviah221@gmail.com>
2025-03-09 18:56:39 -0400

    issue template and fix issue card

--- src/app/(templates)/issue-card.tsx
+++ src/app/(templates)/issue-card.tsx
@@ -8,23 +8,21 @@ interface IssueCard {
 }
 
 export default function IssueCard({article}: IssueCard) {
-    const image = article.Image
-    const title = article.Title
-    const genre = String(article.Content_Type).toUpperCase()
-    const text = article.Text
-    const issue = article.Issue
-    const link = issue.toLowerCase().replaceAll(" ", "-") + "/" + title.toLowerCase().replaceAll(" ", "-").replace(/[^a-zA-Z0-9 -]/g,"")
+    const image = article.image;
+    const title = article.title;
+    const genre = article.genre.toUpperCase();
+    // const link = issue.toLowerCase().replaceAll(" ", "-") + "/" + title.toLowerCase().replaceAll(" ", "-").replace(/[^a-zA-Z0-9 -]/g,"")
     return (
-        <Link href={link}>
+        <Link href='/'>
         <div className={styles.card}>
             <div className={styles.text}>
                 <h2>{title}</h2>
                 <h3>{genre}</h3>
-                <p className={styles.preview} dangerouslySetInnerHTML={{ __html: text.replace(/\\n/g, '\n')}}></p>
+                {/* <p className={styles.preview} dangerouslySetInnerHTML={{ __html: text.replace(/\\n/g, '\n')}}></p> */}
             </div>
             <Image 
                 className={styles.cardImage}
-                src={image ? image : '/text.jpg'}
+                src={image ? image : '/test.jpg'}
                 width={400}
                 height={400}
                 alt='article image'/>
